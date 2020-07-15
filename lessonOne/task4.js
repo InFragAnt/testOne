dd8e07a228e1aba59a5e511c4e3f2ba7a788536d
InFragAnt <schadrin.ant@yandex.ru>
2020-07-15 17:55:58 +0300

    Second commit

---
+++ lessonOne/task4.js
@@ -0,0 +1,14 @@
+var str1='TRVNWL.html';
+var str2='TRVNWL';
+
+function check(str){
+    if(str.endsWith('.html')){
+        console.log('true');
+    }
+    else{
+        console.log('false');
+    }
+}
+
+check(str1);
+check(str2);
---
+++ lessonOne/task6.js
@@ -0,0 +1,13 @@
+var arr=[];
+var str=1, element='1';
+
+for (var i=0; i<5; i++){
+    arr.push(element);
+    str+=1;
+    element=str.toString();
+    for(var i=0; i<=i; i++){
+        element+=str;
+    }
+}
+
+console.log(arr);
