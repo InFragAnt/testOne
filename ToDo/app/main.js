986b9566edc585914eda2922046e4040d83751e8
InFragAnt <schadrin.ant@yandex.ru>
2020-07-18 21:58:17 +0300

    Commit ToDo

--- ToDo/app/main.js
+++ ToDo/app/main.js
@@ -6,8 +6,12 @@ var lists = document.querySelectorAll("li");
 var tipsBtn = document.querySelector(".tipBtn");
 
 var closeBtn = document.querySelector(".closebtn");
+var liUn = document.querySelector(".uncheked");
+/*var liCh = document.querySelector(".cheked"); */
+
 
 var overlay = document.getElementById("overlay");
+var trash = document.getElementById("trash");
 
 tipsBtn.addEventListener("click", function(){
     overlay.style.height = '100%';
@@ -17,3 +21,13 @@ closeBtn.addEventListener("click", function(event) {
     event.preventDefault();
     overlay.style.height = "0";
 });
+
+trash.addEventListener("click", function(){
+    liUn.classList.remove("unchecked");
+    liUn.classList.add("checked");
+});
+
+/*trash.addEventListener("click", function(){
+    liCh.classList.remove("checked");
+    liCh.classList.add("unchecked");
+});*/
