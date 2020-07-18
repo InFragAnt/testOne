4b8ddc2399eaeabff5230d84cb3095d94322bf45
InFragAnt <schadrin.ant@yandex.ru>
2020-07-18 22:31:49 +0300

    Eat calc

---
+++ EatCalc/calculator.js
@@ -0,0 +1,13 @@
+function calculate(){
+
+}
+
+//Hide tips element on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick = function(){
+    calculate();
+}
+
+
